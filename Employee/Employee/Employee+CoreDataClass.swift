c33b57f6777be461d1ae69c11579bf8bffa38841
Vikhyath <vus@Admins-Mac-mini.local>
2018-09-06 16:05:54 +0530

    One table

---
+++ Employee/Employee/Employee+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Employee+CoreDataClass.swift
+//  Employee
+//
+//  Created by Vikhyath on 05/09/18.
+//  Copyright © 2018 Vikhyath. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Employee: NSManagedObject {
+
+}
